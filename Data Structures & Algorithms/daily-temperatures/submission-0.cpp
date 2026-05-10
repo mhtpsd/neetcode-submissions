e4119d5d29824c9d3dfb82421971279241afca42
Mohit Prasad <81853856+mhtpsd@users.noreply.github.com>
2026-05-11 00:24:55 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+
+        vector<int> result(n, 0);
+
+        stack<int> st; // stores indices
+
+        for (int i = 0; i < n; i++) {
+
+            // found warmer day
+            while (!st.empty() &&
+                   temperatures[i] > temperatures[st.top()]) {
+
+                int prevIndex = st.top();
+                st.pop();
+
+                result[prevIndex] = i - prevIndex;
+            }
+
+            st.push(i);
+        }
+
+        return result;
+    }
+};
